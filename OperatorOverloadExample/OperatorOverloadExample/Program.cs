97f6065fb9551edd9fdd7825a3a856b8122ceec7
M Zain <zain.sakhi3@gmail.com>
2025-12-03 00:26:14 +0500

    Add files via upload

---
+++ OperatorOverloadExample/OperatorOverloadExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Rectangle
+    {
+        public int Width;
+        public int Height;
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public static Rectangle operator +(Rectangle react1, Rectangle react2)
+        {
+            Rectangle ReactResult = new Rectangle(react1.Width + react2.Width, react1.Height + react2.Height);
+
+            return ReactResult;
+        }
+
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle react1 = new Rectangle(12, 5);
+            Rectangle react2 = new Rectangle(3, 7);
+
+            Rectangle ReactResult = react1 + react2;
+
+            Console.WriteLine($"'react1(12,10)' + 'react2(5,2)' = 'rectResult({ReactResult.Width},{ReactResult.Height})'");
+
+            Console.ReadKey();
+        }
+    }
+}
